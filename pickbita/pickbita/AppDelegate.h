e4904413a2869f1c267c79b669a55205995d8bb4
pickbita <guanyi.steven.li@gmail.com>
2014-12-13 17:14:47 +0000

    start from begining
    again, try

---
+++ pickbita/pickbita/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pickbita
+//
+//  Created by Peerincle on 13/12/2014.
+//  Copyright (c) 2014 Peerincle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
