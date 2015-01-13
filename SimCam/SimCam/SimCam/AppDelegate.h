471ef35fa72c0d875ac0df15efaf490bfbec4768
pickbita <guanyi.steven.li@gmail.com>
2015-01-13 22:10:07 +0000

    added three button
    Try to figure out how to save photos, but still don’t know.
    1: how to set figure’s info as nil
    2: how to save figure to specific album
    3: how to cut figure

---
+++ SimCam/SimCam/SimCam/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  SimCam
+//
+//  Created by Peerincle on 12/01/2015.
+//  Copyright (c) 2015 Peerincle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ SimCam/SimCam/SimCam/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimCam
+//
+//  Created by Peerincle on 12/01/2015.
+//  Copyright (c) 2015 Peerincle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
